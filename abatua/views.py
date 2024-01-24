3f63ff793bf3968bf5ef0a717167d3ff91c5cba1
Python-David <mrnwobodo@gmail.com>
2024-01-24 14:03:09 +0100

    Views: Dynamically generating products and categories in the store and navbar, configure url.py to recieve catgeory_slug argument to show products assigned to a category

--- abatua/views.py
+++ abatua/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
+from category.models import Category
 from store.models import Product
 
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = get_list_or_404(Product, is_available=True)
+    categories = get_list_or_404(Category)
 
     context = {
         'products': products,
+        'categories': categories,
     }
     return render(request, 'home.html', context)
--- store/views.py
+++ store/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
+from category.models import Category
 from .models import Product
 
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)
+def store(request, category_slug=None):
+    if category_slug:
+        categories = get_list_or_404(Category, slug=category_slug)
+        products = get_list_or_404(Product, is_available=True, category__in=categories)
+    else:
+        categories = Category.objects.all()
+        products = Product.objects.filter(is_available=True)
+
     product_count = len(products)
 
     context = {
         'products': products,
         'product_count': product_count,
+        'categories': categories,
     }
     return render(request, 'store/store.html', context)
+
